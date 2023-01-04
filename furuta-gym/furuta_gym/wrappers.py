78a9ccf575a8d48d5fa0bcb1be67500387fbe6b2
Armand <adpl33@gmail.com>
2023-01-04 19:45:46 +0100

    - fix error where last mcap file did't get closed and got corrupted - include continuity cost in info dict

--- furuta-gym/furuta_gym/wrappers.py
+++ furuta-gym/furuta_gym/wrappers.py
@@ -4,17 +4,17 @@ from pathlib import Path
 from typing import Union
 
 import gym
-from gym.spaces import Box
 import numpy as np
-import wandb
-from mcap_protobuf.writer import Writer
 from furuta_gym.logging.protobuf.pendulum_state_pb2 import PendulumState
+from gym.spaces import Box
+from mcap_protobuf.writer import Writer
+
+import wandb
 
 
 class GentlyTerminating(gym.Wrapper):
-    """
-    This env wrapper sends zero command to the robot when an episode is done.
-    """
+    """This env wrapper sends zero command to the robot when an episode is done."""
+
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
         if done:
@@ -46,17 +46,15 @@ class MCAPLogger(gym.Wrapper):
         if self.use_sim_time:
             time_to_log = self.sim_time
         else:
-            time_to_log = time.time_ns()
+            time_to_log = time.time_ns()  # TODO check that's the right clock
 
         # convert to milliseconds
-        time_to_log = round(time_to_log*1e9)
+        time_to_log = round(time_to_log * 1e9)
 
         self.mcap_writer.write_message(
             topic="/pendulum_state",
             message=PendulumState(**info),
-
-            # TODO this should use the simulation time in the case of a simulation
-            log_time=time_to_log,  #  TODO check that's the right clock
+            log_time=time_to_log,
             publish_time=time_to_log,
         )
 
@@ -68,10 +66,7 @@ class MCAPLogger(gym.Wrapper):
             self.log_dir.mkdir(parents=True)
 
         # close previous log file
-        # TODO unsure how that'll pan out for the last episode
-        if self.mcap_writer is not None:
-            self.mcap_writer.finish()
-            self.output_file.close()
+        self.close_mcap_writer()
 
         # instantiate a new MCAP writer
         fname = f"ep{self.episodes}_{time.strftime('%Y%m%d-%H%M%S')}.mcap"
@@ -87,13 +82,21 @@ class MCAPLogger(gym.Wrapper):
         self.sim_time = 0
         return self.env.reset()
 
+    def close(self):
+        self.close_mcap_writer()
+        return self.env.close()
+
+    def close_mcap_writer(self):
+        if self.mcap_writer is not None:
+            self.mcap_writer.finish()
+            self.output_file.close()
+
 
 class ControlFrequency(gym.Wrapper):
-    """
-    Enforce a sleeping time (dt) between each step.
-    """
+    """Enforce a sleeping time (dt) between each step."""
+
     def __init__(self, env, dt):
-        super(ControlFrequency, self).__init__(env)
+        super().__init__(env)
         self.dt = dt
 
     def step(self, action):
@@ -123,20 +126,17 @@ class ControlFrequency(gym.Wrapper):
 
 
 class HistoryWrapper(gym.Wrapper):
-    """
-    Track history of observations for given amount of steps
-    Initial steps are zero-filled
-    """
-    def __init__(self, env, steps, use_continuity_cost):
-        super(HistoryWrapper, self).__init__(env)
+    """Track history of observations for given amount of steps Initial steps are zero-filled."""
+
+    def __init__(self, env: gym.Env, steps: int, use_continuity_cost: bool):
+        super().__init__(env)
+        assert steps > 1, "steps must be > 1"
         self.steps = steps
         self.use_continuity_cost = use_continuity_cost
 
         # concat obs with action
-        self.step_low = np.concatenate([self.observation_space.low,
-                                        self.action_space.low])
-        self.step_high = np.concatenate([self.observation_space.high,
-                                         self.action_space.high])
+        self.step_low = np.concatenate([self.observation_space.low, self.action_space.low])
+        self.step_high = np.concatenate([self.observation_space.high, self.action_space.high])
 
         # stack for each step
         obs_low = np.tile(self.step_low, (self.steps, 1))
@@ -150,9 +150,11 @@ class HistoryWrapper(gym.Wrapper):
         return [np.zeros_like(self.step_low) for _ in range(self.steps)]
 
     def _continuity_cost(self, obs):
+        # TODO compute continuity cost for all steps and average?
+        # and compare smoothness between training run, and viz smoothness over time
         action = obs[-1][-1]
         last_action = obs[-2][-1]
-        continuity_cost = np.power((action-last_action), 2).sum()
+        continuity_cost = np.power((action - last_action), 2).sum()
 
         return continuity_cost
 
@@ -165,14 +167,15 @@ class HistoryWrapper(gym.Wrapper):
         obs = np.array(self.history)
 
         if self.use_continuity_cost:
-            reward -= self._continuity_cost(obs)
+            continuity_cost = self._continuity_cost(obs)
+            reward -= continuity_cost
+            info["continuity_cost"] = continuity_cost
 
         return obs, reward, done, info
 
     def reset(self):
         self.history = self._make_history()
         self.history.pop(0)
-        obs = np.concatenate([self.env.reset(),
-                              np.zeros_like(self.env.action_space.low)])
+        obs = np.concatenate([self.env.reset(), np.zeros_like(self.env.action_space.low)])
         self.history.append(obs)
         return np.array(self.history)
