7533480dd300216a6085854ad72cc10d727a7da7
Armand <adpl33@gmail.com>
2023-01-04 19:49:36 +0100

    allow passing sim params when instantiating env

--- furuta-gym/furuta_gym/envs/furuta_sim.py
+++ furuta-gym/furuta_gym/envs/furuta_sim.py
@@ -1,17 +1,19 @@
 import gym
 import numpy as np
+from furuta_gym.utils import ALPHA, ALPHA_DOT, THETA, THETA_DOT
 
 from .furuta_base import FurutaBase
 
 
 class FurutaSim(FurutaBase):
+    def __init__(
+        self, fs=50, action_limiter=True, safety_th_lim=1.5, state_limits="low", sim_params=None
+    ):
 
-    def __init__(self, fs=50, action_limiter=True,
-                 safety_th_lim=1.5, state_limits='low'):
-
-        super().__init__(fs, action_limiter, safety_th_lim,
-                         state_limits)
+        super().__init__(fs, action_limiter, safety_th_lim, state_limits)
         self.dyn = QubeDynamics()
+        if sim_params:
+            self.dyn.params = sim_params
 
     def _init_state(self):
         # TODO could also sample from state space
@@ -25,18 +27,17 @@ class FurutaSim(FurutaBase):
         # or maybe it's too slow to move even the simulated pendulum?
         # and maybe it should have a min voltage as well?
         # self._state = np.random.rand(4)  # self.state_space.sample()
-        self._state = \
-            0.01 * np.float32(np.random.randn(self.state_space.shape[0]))
+        self._state = 0.01 * np.float32(np.random.randn(self.state_space.shape[0]))
         # print(self._state)
         self._state = self._update_state(0)
         # print(self._state)
 
     def _update_state(self, a):
         thdd, aldd = self.dyn(self._state, a)
-        self._state[3] += self.timing.dt * aldd  # TODO replace 3 ALPHA_DOT
-        self._state[2] += self.timing.dt * thdd
-        self._state[1] += self.timing.dt * self._state[3]
-        self._state[0] += self.timing.dt * self._state[2]
+        self._state[ALPHA_DOT] += self.timing.dt * aldd
+        self._state[THETA_DOT] += self.timing.dt * thdd
+        self._state[ALPHA] += self.timing.dt * self._state[ALPHA_DOT]
+        self._state[THETA] += self.timing.dt * self._state[THETA_DOT]
         return np.copy(self._state)
 
     def reset(self):
@@ -45,9 +46,8 @@ class FurutaSim(FurutaBase):
 
 
 class Parameterized(gym.Wrapper):
-    """
-    Allow passing new dynamics parameters upon environment reset.
-    """
+    """Allow passing new dynamics parameters upon environment reset."""
+
     def params(self):
         return self.unwrapped.dyn.params
 
@@ -74,25 +74,25 @@ class QubeDynamics:
         # Rotary arm
         self.Mr = 0.095  # mass (kg)
         self.Lr = 0.085  # length (m)
-        self.Dr = 5e-6   # viscous damping (N-m-s/rad), original: 0.0015
+        self.Dr = 5e-6  # viscous damping (N-m-s/rad), original: 0.0015
 
         # Pendulum link
         self.Mp = 0.024  # mass (kg)
         self.Lp = 0.129  # length (m)
-        self.Dp = 1e-6   # viscous damping (N-m-s/rad), original: 0.0005
+        self.Dp = 1e-6  # viscous damping (N-m-s/rad), original: 0.0005
 
         # Init constants
         self._init_const()
 
     def _init_const(self):
         # Moments of inertia
-        Jr = self.Mr * self.Lr ** 2 / 12  # inertia about COM (kg-m^2)
-        Jp = self.Mp * self.Lp ** 2 / 12  # inertia about COM (kg-m^2)
+        Jr = self.Mr * self.Lr**2 / 12  # inertia about COM (kg-m^2)
+        Jp = self.Mp * self.Lp**2 / 12  # inertia about COM (kg-m^2)
 
         # Constants for equations of motion
         self._c = np.zeros(5)
-        self._c[0] = Jr + self.Mp * self.Lr ** 2
-        self._c[1] = 0.25 * self.Mp * self.Lp ** 2
+        self._c[0] = Jr + self.Mp * self.Lr**2
+        self._c[1] = 0.25 * self.Mp * self.Lp**2
         self._c[2] = 0.5 * self.Mp * self.Lp * self.Lr
         self._c[3] = Jp + self._c[1]
         self._c[4] = 0.5 * self.Mp * self.Lp * self.g
@@ -100,7 +100,7 @@ class QubeDynamics:
     @property
     def params(self):
         params = self.__dict__.copy()
-        params.pop('_c')
+        params.pop("_c")
         return params
 
     @params.setter
@@ -120,10 +120,8 @@ class QubeDynamics:
 
         # Calculate vector [x, y] = tau - C(q, qd)
         trq = self.km * (voltage - self.km * thd) / self.Rm
-        c0 = self._c[1] * np.sin(2 * al) * thd * ald \
-            - self._c[2] * np.sin(al) * ald * ald
-        c1 = -0.5 * self._c[1] * np.sin(2 * al) * thd * thd \
-            + self._c[4] * np.sin(al)
+        c0 = self._c[1] * np.sin(2 * al) * thd * ald - self._c[2] * np.sin(al) * ald * ald
+        c1 = -0.5 * self._c[1] * np.sin(2 * al) * thd * thd + self._c[4] * np.sin(al)
         x = trq - self.Dr * thd - c0
         y = -self.Dp * ald - c1
 
