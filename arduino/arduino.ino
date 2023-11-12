e334ad2f7158f96ead0f915b5f9258e1fe577d3b
Armand <adpl33@gmail.com>
2023-11-12 17:54:33 +0100

    setup minimal working protocol + pid

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,151 +1,113 @@
 #include <stdint.h>
 #include <Encoder.h>
 
-const uint8_t MOTOR_DRIVER = 0;  // 0 = MC33926
-const uint8_t MIN1 = 7;
-const uint8_t MIN2 = 8;
-const uint8_t MD2 = 6;
+// Pin definitions
+const uint8_t MOTOR_DRIVER = 0; // 0 = MC33926
+const uint8_t MOTOR_IN1 = 1; // which way to go
+const uint8_t MOTOR_IN2 = 2;
+const uint8_t MOTOR_D2 = A3; // how fast to go
 
-// uint8_t MOTOR_DRIVER = 1; // 1 = TB9051-FTG
-uint8_t PWM1 = NULL;
-uint8_t PWM2 = NULL;
+// uint8_t MOTOR_DRIVER = 1 // 1 = TB9051-FTG
+uint8_t PWM1 = 0;
+uint8_t PWM2 = 0;
 
-// motor encoder
-const uint8_t MOTOR_ENCODER_A = 3;
-const uint8_t MOTOR_ENCODER_B = 4;
+const uint8_t MOTOR_ENC_A = 9;
+const uint8_t MOTOR_ENC_B = 10;
+
+const uint8_t PENDULUM_ENC_A = 7;
+const uint8_t PENDULUM_ENC_B = 8;
+
 const float MOTOR_ENCODER_CPR = 211.2;
-
-// pendulum encoder
-const uint8_t PENDULUM_ENCODER_A = 2;
-const uint8_t PENDULUM_ENCODER_B = 5;
 const float PENDULUM_ENCODER_CPR = 8192;
 
-// protocol
-const int16_t RESET_ENCODERS = 1000;
-const int16_t KILL_MOTOR = 2000;
-const uint8_t ACK = 0x55;
+Encoder motorEncoder(MOTOR_ENC_A, MOTOR_ENC_B);
+Encoder pendulumEncoder(PENDULUM_ENC_A, PENDULUM_ENC_B);
 
 
-void processMotorCommand(int8_t motor_command) {
-  if (MOTOR_DRIVER == 0) {  // MC33926
+void processMotorCommand(int16_t motor_command) {
+  // clamp motor command between -255 and 255
+  if (motor_command > 255) {
+    motor_command = 255;
+  }
+  else if (motor_command < -255) {
+    motor_command = -255;
+  }
+
+  // TODO deadzone
+  uint8_t scale_motor_command = 0;
+  if (motor_command != 0){
+    scale_motor_command = int(0.3 * 255 + 0.7 * abs(motor_command));
+  }
+
+  if (MOTOR_DRIVER == 0) { // MC33926
     if (motor_command > 0) {
-      digitalWrite(MIN1, LOW);
-      digitalWrite(MIN2, HIGH);
-    } else {
-      digitalWrite(MIN1, HIGH);
-      digitalWrite(MIN2, LOW);
+      digitalWrite(MOTOR_IN1, LOW);
+      digitalWrite(MOTOR_IN2, HIGH);
     }
-    analogWrite(MD2, abs(motor_command));
-  } else if (MOTOR_DRIVER == 1) {  // TB9051-FTG
+    else {
+      digitalWrite(MOTOR_IN1, HIGH);
+      digitalWrite(MOTOR_IN2, LOW);
+    }
+    analogWrite(MOTOR_D2, scale_motor_command);
+  }
+  else if (MOTOR_DRIVER == 1) { // TB9051-FTG
     if (motor_command > 0) {
-      analogWrite(PWM1, abs(motor_command));
+      analogWrite(PWM1, scale_motor_command);
       analogWrite(PWM2, 0);
-    } else {
+    }
+    else {
       analogWrite(PWM1, 0);
-      analogWrite(PWM2, abs(motor_command));
+      analogWrite(PWM2, scale_motor_command);
     }
   }
 }
 
-// Encoder motorEncoder(MOTOR_ENCODER_A, MOTOR_ENCODER_B);
-// Encoder pendulumEncoder(PENDULUM_ENCODER_A, PENDULUM_ENCODER_B);
 
 void setup() {
   // setup motor pins
-  if (MOTOR_DRIVER == 0) {
-    pinMode(MIN1, OUTPUT);
-    pinMode(MIN2, OUTPUT);
-    pinMode(MD2, OUTPUT);
-    analogWrite(MD2, 0);  // check that's needed
-  } else {
+  if (MOTOR_DRIVER == 0) { // MC33926
+    pinMode(MOTOR_IN1, OUTPUT);
+    pinMode(MOTOR_IN2, OUTPUT);
+    pinMode(MOTOR_D2, OUTPUT);
+  }
+  else if (MOTOR_DRIVER == 1) { // TB9051-FTG
     pinMode(PWM1, OUTPUT);
     pinMode(PWM2, OUTPUT);
-    analogWrite(PWM1, 0);  // check that's needed
-    analogWrite(PWM2, 0);
   }
+  analogWriteResolution(8);
 
   // setup serial
-  //Serial.begin(921600);
-  // while (!Serial) {}
-  // Serial.setTimeout(10);
+  Serial.begin(57600);
 }
 
 
 void loop() {
-  // wait until we get one whole dummy packet
-  if (Serial.available() > 7) {
-    // read dummy packet
-    unsigned long start = micros();
-    for (int i = 0; i < 8; i++) {
-      Serial.read();
-    }
-    unsigned long elapsed = micros() - start;
-    Serial.print("8x Serial.read() = ");
-    Serial.print(elapsed);
-    Serial.println("us");
+  // if at least two bytes (== one motor command)
+  // in the serial buffer
+  if (Serial.available() >= 2){
+    // read the two bytes
+    uint8_t low_byte = Serial.read();
+    uint8_t high_byte = Serial.read();
+    // combine the two bytes to one int16_t
+    int16_t motor_command = (high_byte << 8) | low_byte;
+    // process the motor command
+    processMotorCommand(motor_command);
 
-    // send back dummy packet
-    start = micros();
-    uint8_t dummy_bit = 0;
-    for (int i = 0; i < 8; i++) {
-      Serial.write(dummy_bit);
-    }
-    elapsed = micros() - start;
-    Serial.print("8x Serial.write() = ");
-    Serial.print(elapsed);
-    Serial.println("us");
+    // read the motor and pendulum encoder values
+    // convert to angles in radians
+    // and send them back as two floats
+
+    // read the motor and pendulum encoder values
+    long motorEncoderValue = motorEncoder.read();
+    long pendulumEncoderValue = pendulumEncoder.read();
+
+    // convert to angles in radians
+    float motorAngle = (2 * PI * motorEncoderValue) / MOTOR_ENCODER_CPR;
+    float pendulumAngle = (2 * PI * pendulumEncoderValue) / PENDULUM_ENCODER_CPR;
+
+    // send them back as two floats
+    Serial.write((uint8_t*)&motorAngle, sizeof(motorAngle));
+    Serial.write((uint8_t*)&pendulumAngle, sizeof(pendulumAngle));
   }
+
 }
-
-
-// void loop() {
-//   if (Serial.available() > 3)
-//   {
-//     for (int i = 0; i< 4; i++) {
-//       char c = Serial.read();
-//     }
-
-//     for (int i = 0; i < 16; i++) {
-//       Serial.write(0);
-//     }
-//   }
-// if (Serial.available() > 1) {
-//   // read two bytes into a 16 bit integer using readBytes
-//   int16_t command;
-//   Serial.readBytes((uint8_t*)&command, 2);
-
-//   // float motor_angle = motorEncoder.read() / MOTOR_ENCODER_CPR * 2 * PI;
-//   // float pendulum_angle = pendulumEncoder.read() / PENDULUM_ENCODER_CPR * 2 * PI;
-
-//   // write 8 empty bytes to serial using Serial write
-//   for (int i = 0; i < 8; i++) {
-//     Serial.write(0);
-//   }
-
-
-// Serial.write((uint8_t*)&motor_angle, sizeof(motor_angle));
-// Serial.write((uint8_t*)&pendulum_angle, sizeof(pendulum_angle));
-
-// if (command == 1000) {
-//   // motorEncoder.write(0);
-//   // pendulumEncoder.write(0);
-//   Serial.write(ACK);
-// }
-// else if (command == 2000) {
-//   processMotorCommand(0);
-//   uint8_t ack = 0x55;
-//   Serial.write(ack);
-// }
-// else {
-//   // processMotorCommand(command);
-
-//   // read system state
-//   float motor_angle = motorEncoder.read() / MOTOR_ENCODER_CPR * 2 * PI;
-//   float pendulum_angle = pendulumEncoder.read() / PENDULUM_ENCODER_CPR * 2 * PI;
-
-//   // send system state, 4 bytes for each float
-//   Serial.write((uint8_t*)&motor_angle, sizeof(motor_angle));
-//   Serial.write((uint8_t*)&pendulum_angle, sizeof(pendulum_angle));
-// }
-// }
-// }
