750afccefa6de7fac7a9757fd9ba505405b19be2
pierfabre <pierfa33@gmail.com>
2024-01-13 00:21:30 +0100

    rework of the control script

---
+++ scripts/control.py
@@ -0,0 +1,121 @@
+import json
+
+import matplotlib
+
+# Tkinter needs to be installed for matplotlib to work
+# sudo apt-get install python3-tk
+matplotlib.use("TkAgg")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from furuta_gym.robot import Robot
+from robot.software.controllers import Controller
+
+
+def read_parameters_file():
+    with open("scripts/configs/parameters.json") as f:
+        parameters = json.load(f)
+    return parameters
+
+
+def has_pendulum_fallen(pendulum_angle: float, parameters: dict):
+    # TODO: Implement in firware.ino for better safety?
+    setpoint = parameters["pendulum_controller"]["setpoint"]
+    angle_threshold = parameters["angle_threshold"]
+    return np.abs(pendulum_angle - setpoint) > angle_threshold
+
+
+def plot_data(actions, motor_angles, pendulum_angles):
+    # plot actions
+    plt.figure(1)
+    plt.plot(actions)
+    plt.title("Actions Over Time")
+    plt.xlabel("Time")
+    plt.ylabel("Action Value")
+    plt.grid(True)
+
+    # plot motor angles
+    plt.figure(2)
+    plt.plot(motor_angles)
+    plt.title("Motor Angles Over Time")
+    plt.xlabel("Time")
+    plt.ylabel("Motor Angle (in radians)")
+    plt.grid(True)
+
+    # plot pendulum angles
+    plt.figure(3)
+    plt.plot(pendulum_angles)
+    plt.title("Pendulum Angles Over Time")
+    plt.xlabel("Time")
+    plt.ylabel("Pendulum Angle (in radians)")
+    plt.grid(True)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Read parameters from the .json file, angles are in degrees
+    parameters = read_parameters_file()
+
+    # Convert the setpoint and the angle threshold to radians
+    parameters["pendulum_controller"]["setpoint"] = np.deg2rad(
+        parameters["pendulum_controller"]["setpoint"]
+    )
+    parameters["angle_threshold"] = np.deg2rad(parameters["angle_threshold"])
+
+    robot = Robot(parameters["device"])
+
+    # Init data lists
+    actions = []
+    motor_angles = []
+    pendulum_angles = []
+
+    # Init pendulum controller
+    pendulum_controller = Controller.build_controller(parameters["pendulum_controller"])
+
+    # Init motor controller
+    motor_controller = Controller.build_controller(parameters["motor_controller"])
+
+    # Reset encoders
+    robot.reset_encoders()
+    print("GO")
+
+    # Wait for user input to start the control loop
+    input()
+
+    # Get the initial motor and pendulum angles
+    motor_angle, pendulum_angle = robot.step(0)
+
+    # Control loop
+    while True:
+        # Compute the motor command from pendulum controller
+        action = pendulum_controller.compute_command(pendulum_angle)
+
+        # Add the motor command from motor controller
+        action += motor_controller.compute_command(motor_angle)
+
+        # Clip the command between -1 and 1
+        action = np.clip(action, -1, 1)
+
+        # Call the step function and get the motor and pendulum angles
+        motor_angle, pendulum_angle = robot.step(action)
+
+        # Take the modulus of the pendulum angle between 0 and 2pi
+        pendulum_angle = np.mod(pendulum_angle, 2 * np.pi)
+
+        # Append the data to the lists
+        actions.append(action)
+        motor_angles.append(motor_angle)
+        pendulum_angles.append(pendulum_angle)
+
+        # Check if pendulum fell
+        if has_pendulum_fallen(pendulum_angle, parameters):
+            print("Pendulum fell!")
+            break
+
+    # Close the serial connection
+    robot.close()
+
+    # Plot data
+    plot_data(actions, motor_angles, pendulum_angles)
