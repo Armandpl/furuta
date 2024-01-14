91c70c791973324d48d48d7dee65a050057b6836
Armand <adpl33@gmail.com>
2024-01-14 17:49:35 +0100

    reorg package structure

--- furuta/__init__.py
+++ furuta/__init__.py
@@ -2,9 +2,9 @@ from gymnasium.envs.registration import register
 
 register(
     id="FurutaReal-v0",
-    entry_point="furuta_gym.envs:FurutaReal",
+    entry_point="furuta.rl.envs.furuta_real:FurutaReal",
 )
 register(
     id="FurutaSim-v0",
-    entry_point="furuta_gym.envs:FurutaSim",
+    entry_point="furuta.rl.envs.furuta_sim:FurutaSim",
 )
