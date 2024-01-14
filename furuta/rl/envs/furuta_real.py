6a7b3b25f3058aa9093bfa87ae32b3fc506b3a28
Armand <adpl33@gmail.com>
2024-01-14 17:45:34 +0100

    use the new robot interface and simplify reset

--- furuta/rl/envs/furuta_real.py
+++ furuta/rl/envs/furuta_real.py
@@ -1,109 +1,64 @@
-import configparser
 import logging
 from time import sleep
+from typing import Optional
 
 import numpy as np
 
-from furuta.common import VelocityFilter
-from furuta.envs.furuta_base import FurutaBase
-from furuta.envs.hardware.robot import FurutaRobot
+from furuta.rl.envs.furuta_base import FurutaBase
+from furuta.robot import Robot
+from furuta.utils import ALPHA, ALPHA_DOT, THETA, THETA_DOT, VelocityFilter
+
+MAX_RESET_TIME = 5  # seconds
 
 
 class FurutaReal(FurutaBase):
     def __init__(
         self,
-        fs=100,
-        fs_ctrl=100,
-        action_limiter=False,
-        safety_th_lim=1.5,
-        reward="simple",
-        state_limits="low",
-        config_file="robot.ini",
+        control_freq=100,
+        reward="alpha_theta",
+        state_limits=None,
+        usb_device="/dev/ttyACM0",
     ):
-        super().__init__(fs, fs_ctrl, action_limiter, safety_th_lim, reward, state_limits)
+        super().__init__(control_freq, reward, state_limits)
 
-        self.robot = FurutaRobot()
+        self.robot = Robot(usb_device)
 
         self.vel_filt = VelocityFilter(2, dt=self.timing.dt)
 
-        self._state = self._read_state()
+        self._update_state(0.0)
 
     def _update_state(self, action):
-        self.robot.set_motor_voltage(action[0])
-
-        state = self._read_state()
-
-        return state
-
-    def _read_state(self):
-        # pendulum_deg_per_count = 360/self.pendulum_CPR
-        # p_count = self.pendulum_enc.readCounter()
-        # # p_count_modulo = p_count % self.pendulum_CPR
-        # pendulum_angle = pendulum_deg_per_count * p_count
-        # # pendulum_angle = (pendulum_angle + 180) % 360
-        # pendulum_angle = pendulum_angle * np.pi / 180
-
-        # motor_deg_per_count = 360/self.motor_CPR
-        # m_count = self.motor_enc.readCounter()
-        # m_count_modulo = m_count % self.motor_CPR
-        # motor_angle = m_count_modulo * motor_deg_per_count
-        # motor_angle = (motor_angle + 180) % 360 - 180
-        # motor_angle = motor_angle * np.pi / 180
-
-        motor_angle, pendulum_angle = self.robot.read_state()
+        motor_angle, pendulum_angle = self.robot.step(action)
 
         # motor_angle: theta, pendulum angle: alpha
         pos = np.array([motor_angle, pendulum_angle], dtype=np.float32)
-        vel = self.vel_filt(pos)  # TODO understand unit: rad/s ?
+        vel = self.vel_filt(pos)
         state = np.concatenate([pos, vel])
+        self._state = state
 
-        return state
-
-    def get_state(self):
-        return self._state
-
-    def _reset_pendulum(self, tolerance=10, still_time=1, clear=True):
-        pass
-
-    def reset(self):
+    def reset(
+        self,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+    ):
         logging.info("Reset env...")
-        # reset pendulum
-        self._reset_pendulum(40, 0.5, False)
-        # reset motor
-        logging.debug("Reset motor")
-        while True:
-            state = self._read_state() * 180 / np.pi
-            motor_angle = state[0]
-            motor_speed = state[2]
 
-            speed = 0.3
-            if abs(motor_angle) < 10:
+        # wait for pendulum to fall back to start position
+        reset_time = 0
+        while np.abs(self._state[ALPHA_DOT]) > 0.1 and reset_time < MAX_RESET_TIME:
+            sleep(0.01)
+            self._update_state(0.0)
+            reset_time += 0.01
 
-                if abs(motor_speed) > 10:
-                    # braking
-                    if motor_angle <= 0:
-                        self.motor.set_speed(-0.3)
-                    elif motor_angle > 0:
-                        self.motor.set_speed(0.3)
+        if reset_time >= MAX_RESET_TIME:
+            logging.error("Reset timeout")
 
-                    sleep(10 / 100)
-
-                    self.motor.set_speed(0)
-
-                self.motor.set_speed(0)
-
-                break
-            elif motor_angle >= 0:
-                self.motor.set_speed(-speed)
-            elif motor_angle < 0:
-                self.motor.set_speed(speed)
-
-        # wait for pendulum to reset to start position
-        self._reset_pendulum(10, 1, True)
+        # reset both encoder, motor back to pos=0
+        self.robot.reset_encoders()
 
         logging.info("Reset done")
-        self._state = self._read_state()
-        return self.get_obs()
+        self._update_state(0.0)
+        return self.get_obs(), {}
 
     # TODO: override parent render function
     # replace by taking webcam snapshot
@@ -113,5 +68,4 @@ class FurutaReal(FurutaBase):
     #     raise NotImplementedError
 
     def close(self):
-        # self.motor.close()
-        self.robot.arduino.close()
+        self.robot.close()
