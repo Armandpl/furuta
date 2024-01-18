47b3d75bb6d81d194f5b9c52453a86f9545b0b16
Armand <adpl33@gmail.com>
2024-01-18 16:15:47 +0100

    remove useless code

--- furuta/rl/__init__.py
+++
@@ -1,3 +0,0 @@
-# from furuta_gym.envs.furuta_base import FurutaBase   # noqa F420
-# from furuta_gym.envs.furuta_real import FurutaReal  # noqa F420
-# from furuta_gym.envs.furuta_sim import FurutaSim   # noqa F420
