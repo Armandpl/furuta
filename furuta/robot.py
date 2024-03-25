93b4cfd504847f0d0dde959c3d8252695dc4c25b
armandpl <adpl33@gmail.com>
2024-03-25 17:45:33 +0100

    bring back quanser sim, take reduction ratio in account

--- furuta/robot.py
+++ furuta/robot.py
@@ -173,3 +173,135 @@ class PendulumDynamics:
         self.th_ddot = th_ddot
         # return np.array([phi_dot, th_dot, phi_ddot, th_ddot])
         return phi_ddot, th_ddot
+
+
+class QubeDynamics:
+    """Solve equation M qdd + C(q, qd) = tau for qdd."""
+
+    def __init__(
+        self,
+        g=9.81,
+        # Motor
+        Rm=8.4,  # reskstance (rated voltage/stall current)
+        Rm_std=0.0,
+        V=12.0,  # nominal voltage
+        reduction_ratio=1.0,
+        km=0.042,  # back-emf constant (V-s/rad) = (rated voltage / no load speed)
+        km_std=0.0,
+        # Rotary Arm
+        Mr=0.095,  # mass (kg)
+        Mr_std=0.0,
+        Lr=0.085,  # length (m)
+        Lr_std=0.0,
+        Dr=5e-6,  # viscous damping (N-m-s/rad), original: 0.0015
+        Dr_std=0.0,
+        # Pendulum Link
+        Mp=0.024,  # mass (kg)
+        Mp_std=0.0,
+        Lp=0.129,  # length (m)
+        Lp_std=0.0,
+        Dp=1e-6,  # viscous damping (N-m-s/rad), original: 0.0005
+        Dp_std=0.0,
+    ):
+        # Gravity
+        self.g = g
+
+        # Motor
+        self.Rm_mean = Rm
+        self.Rm_std = Rm_std
+        self.V = V
+        self.reduction_ratio = reduction_ratio
+
+        self.km_mean = km
+        self.km_std = km_std
+
+        # Rotary arm
+        self.Mr_mean = Mr
+        self.Mr_std = Mr_std
+        self.Lr_mean = Lr
+        self.Lr_std = Lr_std
+        self.Dr_mean = Dr
+        self.Dr_std = Dr_std
+
+        # Pendulum link
+        self.Mp_mean = Mp
+        self.Mp_std = Mp_std
+        self.Lp_mean = Lp
+        self.Lp_std = Lp_std
+        self.Dp_mean = Dp
+        self.Dp_std = Dp_std
+
+        self.randomize()
+
+        # Init constants
+        self._init_const()
+
+    def randomize(self):
+        self.Rm = np.random.normal(self.Rm_mean, self.Rm_std)
+        self.km = np.random.normal(self.km_mean, self.km_std)
+        self.Mr = np.random.normal(self.Mr_mean, self.Mr_std)
+        self.Lr = np.random.normal(self.Lr_mean, self.Lr_std)
+        self.Dr = np.random.normal(self.Dr_mean, self.Dr_std)
+        self.Mp = np.random.normal(self.Mp_mean, self.Mp_std)
+        self.Lp = np.random.normal(self.Lp_mean, self.Lp_std)
+        self.Dp = np.random.normal(self.Dp_mean, self.Dp_std)
+
+    def _init_const(self):
+        # Moments of inertia
+        Jr = self.Mr * self.Lr**2 / 12  # inertia about COM (kg-m^2)
+        Jp = self.Mp * self.Lp**2 / 12  # inertia about COM (kg-m^2)
+
+        # Constants for equations of motion
+        self._c = np.zeros(5)
+        self._c[0] = Jr + self.Mp * self.Lr**2
+        self._c[1] = 0.25 * self.Mp * self.Lp**2
+        self._c[2] = 0.5 * self.Mp * self.Lp * self.Lr
+        self._c[3] = Jp + self._c[1]
+        self._c[4] = 0.5 * self.Mp * self.Lp * self.g
+
+    @property
+    def params(self):
+        params = self.__dict__.copy()
+        params.pop("_c")
+        return params
+
+    @params.setter
+    def params(self, params):
+        self.__dict__.update(params)
+        self._init_const()
+
+    def __call__(self, state, action):
+        # """
+        # action between 0 and 1, maps to +V and -V
+        # """
+        th, al, thd, ald = state
+        voltage = action * self.V
+
+        # Precompute some values
+        sin_al = np.sin(al)
+        sin_2al = np.sin(2 * al)
+        cos_al = np.cos(al)
+
+        # Define mass matrix M = [[a, b], [b, c]]
+        a = self._c[0] + self._c[1] * sin_al**2
+        b = self._c[2] * cos_al
+        c = self._c[3]
+        d = a * c - b * b
+
+        # Calculate vector [x, y] = tau - C(q, qd)
+        trq = (
+            self.reduction_ratio
+            * self.km
+            * (voltage - self.km * thd * self.reduction_ratio)
+            / self.Rm
+        )
+        c0 = self._c[1] * sin_2al * thd * ald - self._c[2] * sin_al * ald * ald
+        c1 = -0.5 * self._c[1] * sin_2al * thd * thd + self._c[4] * sin_al
+        x = trq - self.Dr * thd - c0
+        y = -self.Dp * ald - c1
+
+        # Compute M^{-1} @ [x, y]
+        thdd = (c * x - b * y) / d
+        aldd = (a * y - b * x) / d
+
+        return thdd, aldd
