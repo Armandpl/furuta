68accd51f22636191a1b7a67ba67596c6f3835be
pierfabre <pierfa33@gmail.com>
2024-10-25 19:31:12 +0200

    allow user to choose between 2D and 3D viewer via argparse

--- furuta/viewer.py
+++ furuta/viewer.py
@@ -1,5 +1,5 @@
 import time
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 import numpy as np
 import pinocchio as pin
@@ -10,40 +10,43 @@ from furuta.logger import SimpleLogger
 from furuta.utils import ALPHA, THETA
 
 
-class AbstractViewer:
+class AbstractViewer(ABC):
+    @classmethod
     @abstractmethod
-    def __init__(self):
+    def __init__(cls):
         pass
 
+    @classmethod
     @abstractmethod
-    def display(self):
+    def display(cls, state: np.ndarray) -> (np.ndarray | None):
         pass
 
+    @classmethod
     @abstractmethod
-    def close(self):
+    def close(cls):
         pass
 
 
 class Viewer3D(AbstractViewer):
-    def __init__(self, robot: pin.RobotWrapper = None):
-        self.robot = robot
-        self.viewer = Viewer()
+    def __init__(cls, robot: pin.RobotWrapper = None):
+        cls.robot = robot
+        cls.viewer = Viewer()
         # Attach the robot to the viewer scene
-        self.robot.setVisualizer(Panda3dVisualizer())
-        self.robot.initViewer(viewer=self.viewer)
-        self.robot.loadViewerModel(group_name=self.robot.model.name)
+        cls.robot.setVisualizer(Panda3dVisualizer())
+        cls.robot.initViewer(viewer=cls.viewer)
+        cls.robot.loadViewerModel(group_name=cls.robot.model.name)
 
-    def display(self, q: np.ndarray):
-        self.robot.display(q)
-        return self.viewer.get_screenshot(requested_format="RGB")
+    def display(cls, state: np.ndarray) -> np.ndarray:
+        cls.robot.display(state)
+        return cls.viewer.get_screenshot(requested_format="RGB")
 
-    def close(self):
-        self.viewer.close()
+    def close(cls):
+        cls.viewer.close()
 
-    def animate(self, times: np.ndarray, states: np.ndarray):
+    def animate(cls, times: np.ndarray, states: np.ndarray):
         # Initial state
         q = np.array(states)[:, :2]
-        self.display(q[0])
+        cls.display(q[0])
         time.sleep(1.0)
         tic = time.time()
         for i in range(1, len(times)):
@@ -51,53 +54,53 @@ class Viewer3D(AbstractViewer):
             time.sleep(max(0, times[i] - times[i - 1] - (toc - tic)))
             tic = time.time()
             if i % 10 == 0:
-                self.display(q[i])
+                cls.display(q[i])
 
-    def animate_log(self, log: SimpleLogger):
-        self.animate(log.times, log.states)
+    def animate_log(cls, log: SimpleLogger):
+        cls.animate(log.times, log.states)
 
 
 class Viewer2D(AbstractViewer):
-    def __init__(self, render_fps: int, render_mode: str = None):
-        self.render_fps = render_fps
-        self.render_mode = render_mode
+    def __init__(cls, render_fps: int = 50, render_mode: str = None):
+        cls.render_fps = render_fps
+        cls.render_mode = render_mode
 
-        self.screen_width = 600
-        self.screen_height = 400
-        self.screen = None
-        self.clock = None
+        cls.screen_width = 600
+        cls.screen_height = 400
+        cls.screen = None
+        cls.clock = None
 
-    def display(self, state):
+    def display(cls, state: np.ndarray):
         # https://github.com/Farama-Foundation/Gymnasium/blob/6baf8708bfb08e37ce3027b529193169eaa230fd/gymnasium/envs/classic_control/cartpole.py#L229
         import pygame
         from pygame import gfxdraw
 
-        if self.screen is None:
+        if cls.screen is None:
             pygame.init()
-            if self.render_mode == "human":
+            if cls.render_mode == "human":
                 pygame.display.init()
-                self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-            elif self.render_mode == "rgb_array":
-                self.screen = pygame.Surface((self.screen_width, self.screen_height))
+                cls.screen = pygame.display.set_mode((cls.screen_width, cls.screen_height))
+            elif cls.render_mode == "rgb_array":
+                cls.screen = pygame.Surface((cls.screen_width, cls.screen_height))
             else:
-                print(f"invalid render mode : {self.render_mode}")
+                print(f"invalid render mode : {cls.render_mode}")
                 return
 
-        if self.clock is None:
-            self.clock = pygame.time.Clock()
+        if cls.clock is None:
+            cls.clock = pygame.time.Clock()
 
         if state is None:
             return None
 
         world_width = 2 * np.pi
-        scale = self.screen_width / world_width
+        scale = cls.screen_width / world_width
         polewidth = 10.0
         polelen = scale * (2 * 0.5)  # TODO use the right pole len?
         cartwidth = 50.0
         cartheight = 30.0
 
-        self.surf = pygame.Surface((self.screen_width, self.screen_height))
-        self.surf.fill((255, 255, 255))
+        cls.surf = pygame.Surface((cls.screen_width, cls.screen_height))
+        cls.surf.fill((255, 255, 255))
 
         l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
         axleoffset = cartheight / 4.0
@@ -108,8 +111,8 @@ class Viewer2D(AbstractViewer):
         carty = 100  # TOP OF CART
         cart_coords = [(l, b), (l, t), (r, t), (r, b)]
         cart_coords = [(c[0] + cartx, c[1] + carty) for c in cart_coords]
-        gfxdraw.aapolygon(self.surf, cart_coords, (0, 0, 0))
-        gfxdraw.filled_polygon(self.surf, cart_coords, (0, 0, 0))
+        gfxdraw.aapolygon(cls.surf, cart_coords, (0, 0, 0))
+        gfxdraw.filled_polygon(cls.surf, cart_coords, (0, 0, 0))
 
         l, r, t, b = (
             -polewidth / 2,
@@ -123,38 +126,38 @@ class Viewer2D(AbstractViewer):
             coord = pygame.math.Vector2(coord).rotate_rad(state[ALPHA] + np.pi)
             coord = (coord[0] + cartx, coord[1] + carty + axleoffset)
             pole_coords.append(coord)
-        gfxdraw.aapolygon(self.surf, pole_coords, (202, 152, 101))
-        gfxdraw.filled_polygon(self.surf, pole_coords, (202, 152, 101))
+        gfxdraw.aapolygon(cls.surf, pole_coords, (202, 152, 101))
+        gfxdraw.filled_polygon(cls.surf, pole_coords, (202, 152, 101))
 
         gfxdraw.aacircle(
-            self.surf,
+            cls.surf,
             int(cartx),
             int(carty + axleoffset),
             int(polewidth / 2),
             (129, 132, 203),
         )
         gfxdraw.filled_circle(
-            self.surf,
+            cls.surf,
             int(cartx),
             int(carty + axleoffset),
             int(polewidth / 2),
             (129, 132, 203),
         )
 
-        gfxdraw.hline(self.surf, 0, self.screen_width, carty, (0, 0, 0))
+        gfxdraw.hline(cls.surf, 0, cls.screen_width, carty, (0, 0, 0))
 
-        self.surf = pygame.transform.flip(self.surf, False, True)
-        self.screen.blit(self.surf, (0, 0))
-        if self.render_mode == "human":
+        cls.surf = pygame.transform.flip(cls.surf, False, True)
+        cls.screen.blit(cls.surf, (0, 0))
+        if cls.render_mode == "human":
             pygame.event.pump()
-            self.clock.tick(self.render_fps)
+            cls.clock.tick(cls.render_fps)
             pygame.display.flip()
 
-        elif self.render_mode == "rgb_array":
-            return np.transpose(np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2))
+        elif cls.render_mode == "rgb_array":
+            return np.transpose(np.array(pygame.surfarray.pixels3d(cls.screen)), axes=(1, 0, 2))
 
-    def close(self):
-        if self.screen is not None:
+    def close(cls):
+        if cls.screen is not None:
             import pygame
 
             pygame.display.quit()
--- scripts/replay_mcap.py
+++ scripts/replay_mcap.py
@@ -1,23 +1,32 @@
+import argparse
 import time
 
 import numpy as np
 from mcap_protobuf.reader import read_protobuf_messages
 
-from furuta.envs.furuta_base import CartPoleSwingUpViewer
+from furuta.robot import RobotModel
+from furuta.viewer import Viewer2D, Viewer3D
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--type", required=False, default="2D", choices=("2D", "3D"))
+parser.add_argument("-f", "--file_path", required=True)
 
 if __name__ == "__main__":
-    # TODO use smth like argparse to choose the file
-    # or a text interface
-    mcap_path = "../data/3qhetqlp/ep266_20221201-012458.mcap"
-    viewer = CartPoleSwingUpViewer(world_width=5)
+    args = parser.parse_args()
+    if args.type == "2D":
+        viewer = Viewer2D(render_fps=30, render_mode="rgb_array")
+    else:
+        viewer = Viewer3D(RobotModel.robot)
+
     # TODO get dt from the mcap file
     dt = 0.02
 
-    for msg in read_protobuf_messages(mcap_path, log_time_order=True):
+    for msg in read_protobuf_messages(args.file_path, log_time_order=True):
         p = msg.proto_msg
         state = np.array(
             [p.motor_angle, p.pendulum_angle, p.motor_angle_velocity, p.pendulum_angle_velocity]
         )
-        viewer.update(state)
-        viewer.render(return_rgb_array=False)
+        viewer.display(state)
         time.sleep(dt)
+
+    viewer.close()
