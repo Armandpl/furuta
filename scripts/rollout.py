103187841fec76b4ffe958bc8859b7bb85598d95
pierfabre <pierfa33@gmail.com>
2024-10-25 19:42:28 +0200

    use argparse for rollout script

--- scripts/rollout.py
+++ scripts/rollout.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import numpy as np
@@ -7,43 +8,50 @@ from furuta.robot import RobotModel
 from furuta.sim import SimulatedRobot
 from furuta.viewer import Viewer3D
 
-LOG_DIR = Path("../logs/rollout/")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-d", "--dir", default="../logs/rollout/", required=False, help="Log destination directory"
+)
 
-# Time constants
-t_final = 3.0
-control_freq = 100  # Hz
-time_step = 1 / control_freq
+if __name__ == "__main__":
 
-times = np.arange(0, t_final, time_step)
+    args = parser.parse_args()
 
-# Set the initial state
-init_state = np.array([0.0, 3.14, 0.0, 0.0])
+    # Time constants
+    t_final = 3.0
+    control_freq = 100  # Hz
+    time_step = 1 / control_freq
 
-# Robot
-robot = RobotModel.robot
+    times = np.arange(0, t_final, time_step)
 
-# Create the simulation
-sim = SimulatedRobot(robot, init_state, dt=1e-5)
+    # Set the initial state
+    init_state = np.array([0.0, 3.14, 0.0, 0.0])
 
-# Create the data logger
-logger = SimpleLogger()
-logger.update(time=0.0, state=init_state)
+    # Robot
+    robot = RobotModel.robot
 
-robot_viewer = Viewer3D(robot)
+    # Create the simulation
+    sim = SimulatedRobot(robot, init_state, dt=1e-5)
 
-# Rollout
-u = 0.0
-for i, t in enumerate(times[1:]):
-    motor_angle, pendulum_angle = sim.step(u, time_step)
-    motor_speed = (motor_angle - logger.states[-1][0]) / time_step
-    pendulum_speed = (pendulum_angle - logger.states[-1][1]) / time_step
-    logger.update(t, np.array([motor_angle, pendulum_angle, motor_speed, pendulum_speed]))
+    # Create the data logger
+    logger = SimpleLogger()
+    logger.update(time=0.0, state=init_state)
 
-# Save log and plot
-logger.plot()
-logger.show()
-logger.save(LOG_DIR)
+    robot_viewer = Viewer3D(robot)
 
-# Animate
-robot_viewer = Viewer3D(robot)
-robot_viewer.animate_log(logger)
+    # Rollout
+    u = 0.0
+    for i, t in enumerate(times[1:]):
+        motor_angle, pendulum_angle = sim.step(u, time_step)
+        motor_speed = (motor_angle - logger.states[-1][0]) / time_step
+        pendulum_speed = (pendulum_angle - logger.states[-1][1]) / time_step
+        logger.update(t, np.array([motor_angle, pendulum_angle, motor_speed, pendulum_speed]))
+
+    # Save log and plot
+    logger.plot()
+    logger.show()
+    logger.save(Path(args.dir))
+
+    # Animate
+    robot_viewer = Viewer3D(robot)
+    robot_viewer.animate_log(logger)
