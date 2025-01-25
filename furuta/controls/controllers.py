965c2797d8aa9bfbe7e4b6b6e70fec83e12c43bc
pierfabre <pierfa33@gmail.com>
2025-01-25 15:40:46 +0100

    add NMPC swing up controller using crocoddyl

--- furuta/controls/controllers.py
+++ furuta/controls/controllers.py
@@ -1,26 +1,17 @@
+import typing as tp
 from abc import ABC, abstractmethod
 
+import crocoddyl
+import mim_solvers
 import numpy as np
+import pinocchio as pin
 from simple_pid import PID
 
 
 class Controller(ABC):
     @abstractmethod
-    def compute_command(self, position: float):
+    def compute_command(self, state):
         pass
-
-    @staticmethod
-    def build_controller(parameters: dict):
-        controller_type = parameters["controller_type"]
-        # match controller_type:
-        #     case "PIDController":
-        #         return PIDController(parameters)
-        #     case _:
-        #         raise ValueError(f"Invalid controller type: {controller_type}")
-        if controller_type == "PIDController":
-            return PIDController(parameters)
-        else:
-            raise ValueError(f"Invalid controller type: {controller_type}")
 
 
 class PIDController(Controller):
@@ -43,3 +34,138 @@ class PIDController(Controller):
 
     def compute_command(self, position: float):
         return self.pid(position)
+
+
+class SwingUpController(Controller):
+    class FurutaActuationModel(crocoddyl.ActuationModelAbstract):
+        # TODO : Implement in c++ if too slow
+        def __init__(self, state):
+            nu = 1  # Control dimension
+            crocoddyl.ActuationModelAbstract.__init__(self, state, nu=nu)
+
+        def calc(self, data, x, u):
+            assert len(data.tau) == 2
+            # Map the control dimensions to the joint torque
+            data.tau[0] = u
+            data.tau[1] = 0  # Underactuated joint
+
+        def calcDiff(self, data, x, u):
+            # Specify the actuation jacobian
+            data.dtau_du[0] = 1
+            data.dtau_du[1] = 0
+
+    def __init__(self, robot: pin.RobotWrapper, parameters: tp.Dict, xref: np.ndarray):
+
+        # Read parameters
+        control_freq: float = parameters["control_frequency"]
+        t_final: float = parameters["t_final"]
+        self.u_lim: float = parameters["u_lim"]
+        constraints_type: str = parameters["constraints_type"]
+        self.solver_type: str = parameters["solver_type"]
+
+        # Time variables
+        dt = 1 / control_freq  # Time step
+        self.T = int(t_final * control_freq)
+
+        # Instantiate robot as a pinocchio RobotWrapper
+        self.robot = robot
+
+        # State
+        state = crocoddyl.StateMultibody(robot.model)
+
+        # Actuation model
+        actuationModel = self.FurutaActuationModel(state)
+
+        # State Residual
+        stateResidual = crocoddyl.ResidualModelState(state, xref=xref, nu=actuationModel.nu)
+        stateCostModel = crocoddyl.CostModelResidual(state, stateResidual)
+
+        # Control Residual
+        controlResidual = crocoddyl.ResidualModelControl(state, nu=actuationModel.nu)
+        if constraints_type == "SOFT":
+            bounds = crocoddyl.ActivationBounds(np.array([self.u_lim]), np.array([self.u_lim]))
+            activation = crocoddyl.ActivationModelQuadraticBarrier(bounds)
+            controlCost = crocoddyl.CostModelResidual(
+                state, residual=controlResidual, activation=activation
+            )
+        else:
+            controlCost = crocoddyl.CostModelResidual(state, residual=controlResidual)
+
+        # Constraints
+        constraintManager = crocoddyl.ConstraintModelManager(state, nu=actuationModel.nu)
+        if constraints_type == "HARD":
+            constraint = crocoddyl.ConstraintModelResidual(
+                state,
+                controlResidual,
+                np.array([-self.u_lim, -self.u_lim]),
+                np.array([self.u_lim, self.u_lim]),
+            )
+            constraintManager.addConstraint("control_constraint", constraint)
+
+        # Running cost
+        runningCostModel = crocoddyl.CostModelSum(state, nu=actuationModel.nu)
+        # runningCostModel.addCost("state_cost", cost=stateCostModel, weight=1e-6)
+        # runningCostModel.addCost("control_cost", cost=controlCost, weight=1e-3)
+
+        # Terminal cost
+        terminalCostModel = crocoddyl.CostModelSum(state, nu=actuationModel.nu)
+        terminalCostModel.addCost("state_cost", cost=stateCostModel, weight=self.T)
+
+        # IAM
+        self.runningModel = crocoddyl.IntegratedActionModelEuler(
+            crocoddyl.DifferentialActionModelFreeFwdDynamics(
+                state, actuationModel, runningCostModel, constraintManager
+            ),
+            dt,
+        )
+        self.terminalModel = crocoddyl.IntegratedActionModelEuler(
+            crocoddyl.DifferentialActionModelFreeFwdDynamics(
+                state, actuationModel, terminalCostModel
+            ),
+            0.0,
+        )
+
+        # Initial state
+        q0 = np.zeros((state.nq + state.nv,))
+
+        # Create the shooting problem
+        self.create_problem(q0)
+
+    def create_problem(self, state: np.ndarray):
+        self.problem = crocoddyl.ShootingProblem(
+            state, [self.runningModel] * self.T, self.terminalModel
+        )
+
+    def get_warm_start(self) -> tp.Tuple[np.ndarray, np.ndarray]:
+        xs = self.solver.xs
+        us = self.solver.us
+        xs[:-1] = xs[1:]
+        xs[-1] = self.solver.xs[-1]
+        us[:-1] = us[1:]
+        us[-1] = self.solver.us[-1]
+        return xs, us
+
+    def compute_command(
+        self, state: np.ndarray, max_iter: float = 500, x_ws=[], u_ws=[], callback=True
+    ) -> float:
+        self.create_problem(state)
+        if self.solver_type == "SQP":
+            self.solver = mim_solvers.SolverSQP(self.problem)
+        elif self.solver_type == "FDDP":
+            self.solver = crocoddyl.SolverFDDP(self.problem)
+        else:
+            raise ValueError(f"Invalid solver type: {self.solver_type}")
+        if callback:
+            callbacks = []
+            callbacks.append(crocoddyl.CallbackVerbose())
+            self.solver.setCallbacks(callbacks)
+        self.solver.solve(x_ws, u_ws, max_iter, False, 1e-5)
+        # Clamp the control signal
+        u = np.clip(self.solver.us[0][0], -self.u_lim, self.u_lim)
+        return u
+
+    def get_trajectoy(self) -> np.ndarray:
+        return self.solver.xs
+
+    def get_command(self) -> np.ndarray:
+        return self.solver.us
---
+++ scripts/nmpc_swing_up.py
@@ -0,0 +1,121 @@
+import argparse
+import time
+from time import strftime
+
+import crocoddyl
+import numpy as np
+
+from furuta.controls.controllers import SwingUpController
+from furuta.controls.filters import VelocityFilter
+from furuta.controls.utils import read_parameters_file
+from furuta.logger import SimpleLogger
+from furuta.robot import RobotModel
+from furuta.sim import SimulatedRobot
+from furuta.utils import State
+from furuta.viewer import Viewer3D
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dir",
+        default="../logs/closed_loop/",
+        required=False,
+        help="Log destination directory",
+    )
+    parser.add_argument(
+        "-t", "--duration", default=3.0, required=False, help="Duration of the simulation"
+    )
+    args = parser.parse_args()
+
+    # Read parameters
+    parameters = read_parameters_file("scripts/configs/parameters.json")["swing_up_controller"]
+
+    # Time constants
+    t_final = parameters["t_final"]  # MPC Time Horizon (s)
+    control_freq = parameters["control_frequency"]  # Hz
+    time_step = 1 / control_freq  # s
+
+    times = np.arange(0, args.duration, time_step)
+
+    # Robot
+    robot = RobotModel.robot
+    # Initial state
+    state = State(0.0, 0.0, 0.0, 0.0)
+    init_state = np.zeros(4)
+    # Desired State
+    x_ref = np.array([0.0, np.pi, 0.0, 0.0])
+
+    # Create the controller
+    controller = SwingUpController(robot, parameters, x_ref)
+
+    # Create the data logger
+    fname = f"{strftime('%Y%m%d-%H%M%S')}.mcap"
+    logger = SimpleLogger(args.dir + fname)
+
+    # Solve the OCP a first time to get the warm start
+    controller.compute_command(init_state)
+
+    # Create the robot viewer
+    robot_viewer = Viewer3D(robot)
+
+    # Display the solution
+    robot_viewer.animate(np.arange(0, t_final, time_step), np.array(controller.solver.xs.tolist()))
+    crocoddyl.plotOCSolution(controller.solver.xs, controller.solver.us)
+
+    # Create the simulated robot
+    sim = SimulatedRobot(robot, init_state, dt=1e-5)
+
+    # Low pass velocity filter
+    motor_speed_filter = VelocityFilter(5, 20.0, control_freq, init_state[2])
+    pendulum_speed_filter = VelocityFilter(2, 49.0, control_freq, init_state[3])
+
+    # Warm start
+    x_ws = controller.solver.xs
+    u_ws = controller.solver.us
+
+    # Start logging
+    logger.start()
+
+    # Run the simulation
+    for t in times:
+        tic = time.time()
+        # Solve the OCP
+        x = np.array(
+            [
+                state.motor_angle,
+                state.pendulum_angle,
+                state.motor_angle_velocity,
+                state.pendulum_angle_velocity,
+            ]
+        )
+        u = controller.compute_command(x, 1, x_ws, u_ws)
+        toc = time.time()
+        compute_time = toc - tic
+        # Simulate the robot
+        desired_state = controller.get_trajectoy()
+        motor_angle, pendulum_angle = sim.step(u, time_step)
+        # Compute speed via finite differences
+        unfiltered_motor_speed = (motor_angle - state.motor_angle) / time_step
+        unfiltered_pendulum_speed = (pendulum_angle - state.pendulum_angle) / time_step
+        # Filter speed
+        motor_speed = motor_speed_filter(unfiltered_motor_speed)
+        pendulum_speed = pendulum_speed_filter(unfiltered_pendulum_speed)
+        # Get the warm start from the controller
+        x_ws, u_ws = controller.get_warm_start()
+        # Log data
+        state = State(motor_angle, pendulum_angle, motor_speed, pendulum_speed, action=u)
+        logger.update(int(t * 1e9), state)
+
+    # Close logger
+    logger.stop()
+
+    # Read log
+    times, states = logger.load()
+
+    # Animate
+    robot_viewer.animate(times, states)
+    robot_viewer.close()
+
+    # Plot
+    logger.plot(times, states)
