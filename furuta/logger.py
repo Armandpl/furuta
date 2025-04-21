1b75b8f6136147bb451f9441493abc4db090489b
pierfabre <pierfa33@gmail.com>
2025-04-21 18:20:39 +0200

    split logger and loader

--- furuta/logger.py
+++ furuta/logger.py
@@ -1,21 +1,16 @@
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Union
 
-import matplotlib.pyplot as plt
 import numpy as np
 from mcap_protobuf.reader import read_protobuf_messages
 from mcap_protobuf.writer import Writer
 
-from furuta.logging.protobuf.pendulum_state_pb2 import PendulumState
-from furuta.utils import STATE, State
+from furuta.state import State
 
 
 class SimpleLogger:
     def __init__(self, log_path: (str | Path)):
-        self.log_path = log_path
-
-    def start(self):
-        self.output_file = open(self.log_path, "wb")
+        self.output_file = open(log_path, "wb")
         self.mcap_writer = Writer(self.output_file)
 
     def stop(self):
@@ -25,40 +20,37 @@ class SimpleLogger:
     def update(self, time_ns: int, state: State):
         self.mcap_writer.write_message(
             topic="/pendulum_state",
-            message=PendulumState(
-                motor_angle=state.motor_angle,
-                pendulum_angle=state.pendulum_angle,
-                motor_angle_velocity=state.motor_angle_velocity,
-                pendulum_angle_velocity=state.pendulum_angle_velocity,
-                reward=state.reward,
-                action=state.action,
-            ),
+            message=state.to_protobuf(),
             log_time=time_ns,
             publish_time=time_ns,
         )
 
-    def load(self) -> tuple[np.ndarray, np.ndarray]:
-        times: List[float] = list()
-        states: List[np.ndarray] = list()
-        for msg in read_protobuf_messages(self.log_path, log_time_order=True):
-            p = msg.proto_msg
-            state = np.array(
-                [
-                    p.motor_angle,
-                    p.pendulum_angle,
-                    p.motor_angle_velocity,
-                    p.pendulum_angle_velocity,
-                    p.reward,
-                    p.action,
-                ]
-            )
-            times.append(float(msg.log_time_ns * 1e-9))
-            states.append(state)
-        return np.array(times), np.array(states)
 
-    def plot(self, times: List[float], states: List[np.ndarray]):
-        for title, idx in STATE.items():
-            plt.figure(idx + 1)
-            plt.plot(times, states[:, idx])
-            plt.title(title)
-        plt.show()
+class Loader:
+    def __init__(self):
+        self.states_dict: Dict[str, List] = {}
+        self.times = []
+
+    def load(self, log_path: Union[str, Path]) -> tuple[np.ndarray, Dict[str, np.ndarray]]:
+        for msg in read_protobuf_messages(log_path, log_time_order=True):
+            proto = msg.proto_msg
+            self.times.append(msg.log_time_ns * 1e-9)
+            state = State.from_protobuf(proto).to_dict()
+            for key, value in state.items():
+                if key not in self.states_dict:
+                    self.states_dict[key] = []
+                self.states_dict[key].append(value)
+
+        times_array = np.array(self.times)
+        states_array_dict = {k: np.array(v) for k, v in self.states_dict.items()}
+        return times_array, states_array_dict
+
+    def get_state(self, signal: str) -> np.ndarray:
+        return np.array(
+            [
+                self.states_dict["motor_position." + signal],
+                self.states_dict["pendulum_position." + signal],
+                self.states_dict["motor_velocity." + signal],
+                self.states_dict["pendulum_velocity." + signal],
+            ]
+        ).T
